27e16b62a894e0cb9bb4bc4707d5065fb78c7da1
Nilan Ziegler <nilan.zieglerlaurent@gmail.com>
2025-07-26 18:43:14 +0200

    Add function to check neighboring cells

---
+++ generator/maze_generator.py
@@ -0,0 +1,68 @@
+from generator.cell import Cell
+import random
+class MazeGenerator():
+    def __init__(self):
+        pass
+
+    def generateMaze(self, width, height):
+        """Generates a maze of given width and height.
+
+        Args:
+            width (int): The width of the maze.
+            height (int): The height of the maze.
+
+        Returns:
+            list[Cell]: A flat list of Cell objects representing the maze.
+        """
+        self.width = width
+        self.height = height
+        self.maze = [Cell(x, y) for y in range(height) for x in range(width)]
+        self.initialize_maze()
+        return self.maze
+    
+    def initialize_maze(self):
+        initial_cell = random.choice(self.maze)
+        self.propagate(initial_cell)
+    
+    def propagate(self, cell):
+        not_visited_neighbors = self.get_not_visited_neighbors(cell)
+        print(f"Visiting cell at ({cell.x}, {cell.y}), unvisited neighbors: {len(not_visited_neighbors)}")
+    
+    def get_not_visited_neighbors(self, cell):
+        """Returns a list of neighboring cells that have not been visited.
+
+        Args:
+            cell (Cell): The current cell.
+
+        Returns:
+            list[Cell]: A list of neighboring cells that have not been visited.
+        """
+        neighbors = []
+
+        # Helper to get cell by coordinates, returns None if out of bounds
+        def get_cell(x, y):
+            if 0 <= x < self.width and 0 <= y < self.height:
+                return self.maze[y * self.width + x]
+            return None
+
+        # Check left neighbor
+        left = get_cell(cell.x - 1, cell.y)
+        if left and not left.visited:
+            neighbors.append(left)
+
+        # Check right neighbor
+        right = get_cell(cell.x + 1, cell.y)
+        if right and not right.visited:
+            neighbors.append(right)
+
+        # Check top neighbor
+        top = get_cell(cell.x, cell.y - 1)
+        if top and not top.visited:
+            neighbors.append(top)
+
+        # Check bottom neighbor
+        bottom = get_cell(cell.x, cell.y + 1)
+        if bottom and not bottom.visited:
+            neighbors.append(bottom)
+
+        return neighbors
