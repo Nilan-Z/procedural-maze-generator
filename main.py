ef7dc702f9e4850fc0e385170fa664882291fd3c
Nilan Ziegler <nilan.zieglerlaurent@gmail.com>
2025-07-29 20:53:27 +0200

    Add call to display_maze() after maze generation in main loop

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from generator.maze_generator import MazeGenerator
+from display.display import Display
 
 if __name__ == "__main__":
     maze_generator = MazeGenerator()
-    maze = maze_generator.generateMaze(10, 10)
+    display = Display()
+    maze = maze_generator.generateMaze(20, 20)
+    display.display_maze(maze_generator.maze, maze_generator.width, maze_generator.height)
+    
