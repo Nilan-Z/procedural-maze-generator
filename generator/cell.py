87349b83b997f0d26b8e1054c5efcc4a2ef24d91
Nilan Ziegler <nilan.zieglerlaurent@gmail.com>
2025-08-03 16:50:47 +0200

    Add simple comment

--- generator/cell.py
+++ generator/cell.py
@@ -1,5 +1,6 @@
 class Cell:
     def __init__(self, x: int, y: int):
+        '''Initialize a Cell instance.'''
         self.x = x
         self.y = y
         self.walls = [True, True, True, True]
